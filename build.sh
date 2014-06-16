b459f99b208a01ff814b95fcd5710a71e573ddea
Reto Kaiser <reto@cargomedia.ch>
2014-06-16 09:44:37 +0200

    Move build.sh to root

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+DIR="$(cd "$(dirname "${0}")" && pwd)"
+
+PACKAGE='gearmand'
+DIR_PACKAGE="${DIR}/packages/${PACKAGE}"
+VERSION=$(cat "${DIR_PACKAGE}/version" | xargs)
+URL=$(cat "${DIR_PACKAGE}/url" | xargs)
+
+cd "${DIR_PACKAGE}"
+rm -rf tmp/ && mkdir tmp/ && cd tmp/
+wget "${URL}"
+unp ${PACKAGE}-${VERSION}.tar.gz
+mv ${PACKAGE}-${VERSION}.tar.gz ${PACKAGE}_${VERSION}.orig.tar.gz
+cd ${PACKAGE}-${VERSION}/
+
+cp -r ../../debian ./debian
+mk-build-deps --install --remove debian/control
+dpkg-buildpackage -us -uc
+
+cd "${DIR_PACKAGE}"
+rm -rf pkg/ && mkdir pkg/
+mv tmp/*.deb pkg/
+rm -rf tmp/
