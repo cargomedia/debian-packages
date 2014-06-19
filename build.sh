d5dfa79151f3a7c86e5dbf8c7fa86ad976ce9bd4
Reto Kaiser <reto@cargomedia.ch>
2014-06-19 19:10:45 +0200

    Install .debs, run tests, remove-and-readd to repo

--- build.sh
+++ build.sh
@@ -23,4 +23,25 @@ rm -rf pkg/ && mkdir pkg/
 mv tmp/*.deb pkg/
 rm -rf tmp/
 
-reprepro -b "${DIR}/repo" includedeb wheezy "${DIR_PACKAGE}/pkg/"*.deb
+DEB_DIR="${DIR_PACKAGE}/pkg"
+
+for DEB_PATH in "${DEB_DIR}/"*.deb; do
+  echo "Installing ${DEB_PATH}..."
+  dpkg -i "${DEB_PATH}"
+done
+
+PATH_TEST="${DIR_PACKAGE}/debian/tests/run-tests"
+if (test -e "${PATH_TEST}"); then
+  echo "Now executing tests..."
+  "${PATH_TEST}"
+fi
+
+PKG_LIST=$(perl -lne '/^Package: (.+)$/ && print $1' "${DIR_PACKAGE}/debian/control")
+for PKG in ${PKG_LIST}; do
+  echo
+  echo "Adding ${PKG} to repo..."
+  if (reprepro -b "${DIR}/repo" list wheezy "${PKG}" | grep -q "${PKG}"); then
+    reprepro -b "${DIR}/repo" remove wheezy "${PKG}"
+  fi
+  reprepro -b "${DIR}/repo" includedeb wheezy "${DEB_DIR}/${PKG}_"*.deb
+done
