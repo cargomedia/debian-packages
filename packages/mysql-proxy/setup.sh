e08704269fd9e863de6f7e82f5cb9e045073c388
Reto Kaiser <reto@cargomedia.ch>
2014-06-25 15:37:31 +0200

    Update to mysql-proxy 0.8.4

--- packages/mysql-proxy/setup.sh
+++ packages/mysql-proxy/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-VERSION="0.8.1"
-wget "https://launchpad.net/mysql-proxy/0.8/${VERSION}/+download/mysql-proxy-${VERSION}.tar.gz"
+VERSION="0.8.4"
+wget "http://dev.mysql.com/get/Downloads/MySQL-Proxy/mysql-proxy-${VERSION}.tar.gz"
 unp ${PACKAGE}-${VERSION}.tar.gz
 mv ${PACKAGE}-${VERSION}.tar.gz ${PACKAGE}_${VERSION}.orig.tar.gz
 cd ${PACKAGE}-${VERSION}/
