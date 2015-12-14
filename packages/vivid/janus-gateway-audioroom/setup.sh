35370da066bae7df44341ceca403e7c0a4eeafd3
Andre Graf <andre@dergraf.org>
2015-12-14 17:53:04 +0100

    initial version of janus audioroom plugin

---
+++ packages/vivid/janus-gateway-audioroom/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+VERSION="0.0.1"
+wget https://github.com/cargomedia/janus-gateway-audioroom/archive/${VERSION}.tar.gz
+unp ${VERSION}.tar.gz
+mv ${VERSION}.tar.gz ${PACKAGE}_${VERSION}.orig.tar.gz
+cd ${PACKAGE}-${VERSION}
