cc4d892eb2db8cf9dd4b195c7a832392a10ef027
Andre Graf <andre@dergraf.org>
2015-12-14 17:27:35 +0100

    use tagged sources

--- packages/vivid/janus-gateway-rtpbroadcast/setup.sh
+++ packages/vivid/janus-gateway-rtpbroadcast/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-git clone "git://github.com/cargomedia/janus-gateway-rtpbroadcast.git"
-tar -cvzf janus-gateway-rtpbroadcast_0.0.1.orig.tar.gz janus-gateway-rtpbroadcast
-cd janus-gateway-rtpbroadcast
+VERSION="0.0.1"
+wget https://github.com/cargomedia/janus-gateway-rtpbroadcast/archive/${VERSION}.tar.gz
+unp ${VERSION}.tar.gz
+mv ${VERSION}.tar.gz ${PACKAGE}_${VERSION}.orig.tar.gz
+cd ${PACKAGE}-${VERSION}
