3b448da0f8814d6083c69b304a8124a1f2937a6f
Andre Graf <andre@dergraf.org>
2015-12-15 13:14:44 +0100

    fix version

--- packages/vivid/janus-gateway-rtpbroadcast/setup.sh
+++ packages/vivid/janus-gateway-rtpbroadcast/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-VERSION="0.0.1"
-wget https://github.com/cargomedia/janus-gateway-rtpbroadcast/archive/${VERSION}.tar.gz
-unp ${VERSION}.tar.gz
-mv ${VERSION}.tar.gz ${PACKAGE}_${VERSION}.orig.tar.gz
+VERSION="0.0.2"
+wget https://github.com/cargomedia/janus-gateway-rtpbroadcast/archive/v${VERSION}.tar.gz
+unp v${VERSION}.tar.gz
+mv v${VERSION}.tar.gz ${PACKAGE}_${VERSION}.orig.tar.gz
 cd ${PACKAGE}-${VERSION}
