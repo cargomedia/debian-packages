b93ba6efcdf361f8a35f5322f9d2becc104a335c
ppp0 <philipp.gysin@cargomedia.ch>
2015-12-23 13:57:50 +0100

    Adapt source format, versioning, add some meaningful tests

--- packages/wheezy/coturn/setup.sh
+++ packages/wheezy/coturn/setup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-VERSION='4.5.0.3-1'
-wget "http://http.debian.net/debian/pool/main/c/coturn/coturn_${VERSION}.debian.tar.xz"
-unp coturn-${VERSION}.tar.xz
-mv coturn-${VERSION}.tar.xz coturn-${VERSION}.orig.tar.xz
-cd coturn-${VERSION}/
+VERSION='4.5.0.3'
+wget "http://http.debian.net/debian/pool/main/c/coturn/coturn_${VERSION}.orig.tar.gz"
+unp coturn_${VERSION}.orig.tar.gz
+cd turnserver-${VERSION}/
