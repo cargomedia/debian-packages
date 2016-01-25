3cbc492552a1dae3a7c467f19921172c0dbe8fa2
ppp0 <philipp.gysin@cargomedia.ch>
2016-01-25 11:15:11 +0100

    ffmpeg, wheezy

---
+++ packages/wheezy/ffmpeg/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+VERSION="2.8.5"
+#wget "https://ffmpeg.org/releases/ffmpeg-${VERSION}.tar.xz"
+unp ../ffmpeg-${VERSION}.tar.xz
+cp ../ffmpeg-${VERSION}.tar.xz ffmpeg_${VERSION}.orig.tar.xz
+cd ffmpeg-${VERSION}/
