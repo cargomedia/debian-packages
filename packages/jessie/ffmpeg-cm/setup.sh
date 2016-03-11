7233d44707fccd0cf2c91a8c85cbaf3f13ca4fc5
ppp0 <philipp.gysin@cargomedia.ch>
2016-03-12 00:07:22 +0100

    Jessie version (sligthly modified patch)

--- packages/jessie/ffmpeg-cm/setup.sh
+++ packages/jessie/ffmpeg-cm/setup.sh
@@ -3,5 +3,5 @@
 VERSION='2.8.6'
 wget "https://ffmpeg.org/releases/ffmpeg-${VERSION}.tar.gz"
 unp ffmpeg-${VERSION}.tar.gz
-mv ffmpeg-${VERSION}.tar.gz ffmpeg_${VERSION}.orig.tar.gz
+mv ffmpeg-${VERSION}.tar.gz ffmpeg-cm_${VERSION}.orig.tar.gz
 cd ffmpeg-${VERSION}/
