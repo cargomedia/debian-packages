75023ffbc0ea1fccf0c9cfbbd9b1f34165832c32
ppp0 <philipp.gysin@cargomedia.ch>
2016-03-21 16:43:30 +0100

    Update janus and plugins for jessie

--- packages/jessie/janus-gateway-rtpbroadcast/setup.sh
+++ packages/jessie/janus-gateway-rtpbroadcast/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION="0.0.10"
+VERSION="0.0.11"
 wget https://github.com/cargomedia/janus-gateway-rtpbroadcast/archive/v${VERSION}.tar.gz
 unp v${VERSION}.tar.gz
 mv v${VERSION}.tar.gz ${PACKAGE}_${VERSION}.orig.tar.gz
