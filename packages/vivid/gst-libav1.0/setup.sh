06eff5fcc309dfedff35b9a46edbbfa797f27427
Chris Stasiak <me@krzysztof-stasiak.com>
2016-04-08 15:21:50 +0200

    Droped gst-libav support

--- packages/vivid/gst-libav1.0/setup.sh
+++ packages/vivid/gst-libav1.0/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION="1.6.1"
+VERSION="1.8.0"
 wget "http://gstreamer.freedesktop.org/src/gst-libav/gst-libav-${VERSION}.tar.xz"
 unp gst-libav-${VERSION}.tar.xz
 mv gst-libav-${VERSION}.tar.xz ${PACKAGE}_${VERSION}.orig.tar.xz
