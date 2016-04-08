cb5061d2421877863bb816d8845c268ac1c98a68
Chris Stasiak <me@krzysztof-stasiak.com>
2016-04-08 15:24:06 +0200

    Upgraded gst-plugins-ugly1.0 to 1.8.0

--- packages/vivid/gst-plugins-ugly1.0/setup.sh
+++ packages/vivid/gst-plugins-ugly1.0/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION="1.6.1"
+VERSION="1.8.0"
 wget "http://gstreamer.freedesktop.org/src/gst-plugins-ugly/gst-plugins-ugly-${VERSION}.tar.xz"
 unp gst-plugins-ugly-${VERSION}.tar.xz
 mv gst-plugins-ugly-${VERSION}.tar.xz ${PACKAGE}_${VERSION}.orig.tar.xz
