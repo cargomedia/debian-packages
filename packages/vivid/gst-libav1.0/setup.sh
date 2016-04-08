c378544f2de20e92ad5320ac2eb354212286a605
Chris Stasiak <me@krzysztof-stasiak.com>
2016-04-08 15:28:51 +0200

    Really dropped gst-libav

--- packages/vivid/gst-libav1.0/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-VERSION="1.8.0"
-wget "http://gstreamer.freedesktop.org/src/gst-libav/gst-libav-${VERSION}.tar.xz"
-unp gst-libav-${VERSION}.tar.xz
-mv gst-libav-${VERSION}.tar.xz ${PACKAGE}_${VERSION}.orig.tar.xz
-cd gst-libav-${VERSION}/
