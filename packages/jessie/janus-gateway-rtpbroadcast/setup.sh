db3d1028bb44f4475f7892e9379c1c575fb7e758
ppp0 <philipp.gysin@cargomedia.ch>
2016-07-04 15:19:32 +0200

    Bump janus-gateway-rtpbroadcast to 0.0.21

--- packages/jessie/janus-gateway-rtpbroadcast/setup.sh
+++ packages/jessie/janus-gateway-rtpbroadcast/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION="0.0.20"
+VERSION="0.0.21"
 wget https://github.com/cargomedia/janus-gateway-rtpbroadcast/archive/v${VERSION}.tar.gz
 unp v${VERSION}.tar.gz
 mv v${VERSION}.tar.gz ${PACKAGE}_${VERSION}.orig.tar.gz
